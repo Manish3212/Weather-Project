b72445bf128181a433131a745f8cfec6e7c631ce
Manish <manishbharambe123@gmail.com>
2023-01-19 16:51:28 +0530

    initial commit

--- app.js
+++ app.js
@@ -26,9 +26,11 @@ https.get(url,function(response){
     const discription=weatherdata.weather[0].description
     const icon=weatherdata.weather[0].icon 
     const imgurl='https://openweathermap.org/img/wn/'+icon+'@2x.png '
-    res.write("<h1>Temperature in " +city+" is:"+temperature+ " degree celcius"+"</h1>")
-    res.write("<p>"+"Weather Status: "+discription+"</p>")
-    res.write("<img src=" + imgurl + ">") 
+    res.write("<div style='background-image:url(https://t3.ftcdn.net/jpg/04/03/02/92/240_F_403029269_KCrGHt5AdtV7GSD2KeP8Wk2PYIbVKlNU.jpg); background-size:cover;height:100vh;text-align:center;'><h1 style='color:white;background-color:gray;text-align:center;font-family:monospace;font-size:2.5rem;border-radius:3rem'>Temperature in " +city+" is : "+temperature+ " degree celcius"+"</h1><p style='font-family:monospace;color:gray;font-size:2rem;text-align:center'>"+"Weather Status: "+discription+"</p> <div style='background-color:gray;margin:0px 80vh;border-radius:3rem;'><img src=" + imgurl + "></div> </div>")
+    res.write("")
+    res.write("")
+    res.write("") 
+    res.write("<div > </div>")
 
     console.log('city: '+city)
     console.log(imgurl)
